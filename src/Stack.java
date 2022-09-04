49e8e616284efaa285ad00db517d8eec2fd72c02
sooye7 <sooye7@naver.com>
2022-09-04 23:01:22 +0900

    [백준 10828] 스택

---
+++ src/Stack.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Stack {
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+        int N=Integer.parseInt(st.nextToken());
+        for(int i=0;i<N;i++){
+
+        }
+        st=new StringTokenizer(br.readLine(), " ");
+
+
+    }
+}
