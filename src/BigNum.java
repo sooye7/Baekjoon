e8d558cf1edeaad2779386c60e3105b3f48d91ab
sooye7 <sooye7@naver.com>
2022-09-29 14:33:01 +0900

    [백준 10757] 큰 수 A+B (기본 수학1) 다른 사람 풀이 추가

--- src/BigNum.java
+++ src/BigNum.java
@@ -1,6 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class BigNum {
@@ -31,6 +30,7 @@ public class BigNum {
             answer.append(c);
         System.out.println(String.valueOf(answer.reverse()));
     }
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     public static void main(String[] args) throws IOException {
         BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine()," ");
@@ -52,5 +52,14 @@ public class BigNum {
         else{
             sumOfAB(a,b);
         }
+
+        // 풀이 2. BigInteger 객체 사용
+        BigInteger A = new BigInteger(st.nextToken());
+        BigInteger B = new BigInteger(st.nextToken());
+
+        bw.write(String.valueOf(A.add(B)));
+        bw.flush();
+        bw.close();
+
     }
 }
