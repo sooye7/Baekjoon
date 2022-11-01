5adfd90be118a3859f84369b8e826136c58064f2
sooye7 <sooye7@naver.com>
2022-11-01 23:09:13 +0900

    [백준 1018] 체스판 다시 칠하기 (브루트포스)

--- src/ColoringChessBoard.java
+++ src/ColoringChessBoard.java
@@ -12,26 +12,51 @@ public class ColoringChessBoard {
         int col=Integer.parseInt(st.nextToken());
         boolean wCheck=true;
         int count=0;
-        for(int i=0;i<row;i++){
-            String str=br.readLine();
-            if(str.charAt(0)=='W')
-                wCheck=true;
-            else
-                wCheck=false;
+        StringBuilder sb=new StringBuilder(br.readLine());
+        wCheck= sb.charAt(0) == 'W';
+        for(int j=0;j<col-1;j++){
+            System.out.println(sb);
+            if(wCheck&&sb.charAt(0)=='W') {
+                count++;
+                sb.replace(0,1,"B");
+            }
+            else if(!wCheck&&sb.charAt(0)=='B'){
+                count++;
+                sb.replace(0,1,"W");
+            }
+            if(sb.charAt(j)==sb.charAt(j+1)) {
+                count++;
+                if(sb.charAt(j+1)=='W')
+                    sb.replace(j+1,j+2,"B");
+                else
+                    sb.replace(j+1,j+2,"W");
+                j++;
+            }
+        }
+        for(int i=0;i<row-1;i++){
+            StringBuilder sb2=new StringBuilder(br.readLine());
+            System.out.println();
+            System.out.println("i"+i);
             for(int j=0;j<col-1;j++){
-                if(wCheck&&str.charAt(0)=='W') {
+                System.out.println(sb2);
+                System.out.println(j);
+                if(wCheck&&sb2.charAt(0)=='W') {
                     count++;
-                    str=str.replaceFirst("W","B");
+                    sb2.replace(0,1,"B");
                 }
-                else if(!wCheck&&str.charAt(0)=='B'){
+                else if(!wCheck&&sb2.charAt(0)=='B'){
                     count++;
-                    str=str.replaceFirst("B","W");
+                    sb2.replace(0,1,"W");
                 }
-                if(str.charAt(j)==str.charAt(j+1)) {
+                if(sb2.charAt(j)==sb2.charAt(j+1)) {
                     count++;
-                    j++;
+                    if(sb2.charAt(j+1)=='W')
+                        sb2.replace(j+1,j+2,"B");
+                    else
+                        sb2.replace(j+1,j+2,"W");
                 }
             }
+            wCheck= sb2.charAt(0) == 'W';
         }
         System.out.println(count);
     }
