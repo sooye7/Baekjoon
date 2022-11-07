0582dd7e33c1d1e78556ce25aa29ddc3d056c1d0
sooye7 <sooye7@naver.com>
2022-11-07 21:25:47 +0900

    [백준 1085] 직사각형에서 탈출 (기하1)

---
+++ src/EscapeRectangle.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class EscapeRectangle {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st=new StringTokenizer(br.readLine()," ");
+        int x=Integer.parseInt(st.nextToken());
+        int y=Integer.parseInt(st.nextToken());
+        int w=Integer.parseInt(st.nextToken());
+        int h=Integer.parseInt(st.nextToken());
+
+        System.out.println(Math.min(Math.min(x,Math.abs(w-x)),Math.min(y,Math.abs(h-y))));
+
+    }
+}
