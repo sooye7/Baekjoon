bd9b08c15f1d33b343f8ec13ad5a6c449030400b
sooye7 <sooye7@naver.com>
2022-11-14 22:40:25 +0900

    [백준 2477] 참외밭 (기하1)

--- src/MelonField2.java
+++ src/MelonField2.java
@@ -11,12 +11,25 @@ public class MelonField2 {
         StringBuilder sb=new StringBuilder();
         int[] len=new int[6];
         int area1=1,area2=0;
+        boolean check=false;
         for(int i=0;i<6;i++){
             StringTokenizer st=new StringTokenizer(br.readLine()," ");
             sb.append(st.nextToken());  // 방향
             len[i]=Integer.parseInt(st.nextToken());
         }
+
         String order=String.valueOf(sb);
+        if (order.contains("13")||order.contains("24")||order.contains("32")||order.contains("41"))
+            check = true;
+        // dirOrder가 존재하지 않는 경우 순서 바꿔줘야 함
+        // 뺄 너비 부분의 방향(dirOrder)이 존재하도록 순서 변경f
+        int index=0;
+        while(!check) {
+            StringBuilder s=sb.delete(0, 1);
+            sb.append(s);
+            if (order.contains("13")||order.contains("24")||order.contains("32")||order.contains("41"))
+                check = true;
+        }
         for(int i=0;i<4;i++){
             if(order.contains(dirOrder[i])){
                 // 전체 너비
