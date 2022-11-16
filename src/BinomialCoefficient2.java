d6334365b171805e217349b2d88fb3307b7d15ef
sooye7 <sooye7@naver.com>
2022-11-16 22:17:53 +0900

    [백준 11051] 이항계수2 (정수론 및 조합론)

--- src/BinomialCoefficient2.java
+++ src/BinomialCoefficient2.java
@@ -11,12 +11,15 @@ public class BinomialCoefficient2 {
         int n=Integer.parseInt(st.nextToken());
         int k=Integer.parseInt(st.nextToken());
         int[][] dp=new int[n+1][n+1];
-        for(int i=1;i<=n;i++)
-            dp[i][1] = i;
 
-        for(int i=2;i<=n;i++)
-            for(int j=2;j<=i;j++)
-                dp[i][j]=(dp[i-1][j-1]+dp[i-1][j])%10007;
+        for(int i=0;i<=n;i++) {
+            for (int j = 0; j <= i; j++) {
+                if(i==j||j==0)
+                    dp[i][j]=1;
+                else
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % 10007;
+            }
+        }
         // dp
         System.out.println(dp[n][k]);
 
